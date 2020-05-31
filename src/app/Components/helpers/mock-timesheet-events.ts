2ad43c8681276c9c364aa5fa6bb687d56f85463d
ReisEdgar <edgar.reis447@gmail.com>
2020-05-31 15:21:39 +0300

    update mock date

--- src/app/Components/helpers/mock-timesheet-events.ts
+++ src/app/Components/helpers/mock-timesheet-events.ts
@@ -1,18 +1,35 @@
 import { Day } from '../timesheet-widget/api-models/day';
+import { environment } from 'src/environments/environment';
 
-let day7 = new Date();
-let day6 = new Date(new Date().setDate(new Date().getDate() - 1))
-let day5 = new Date(new Date().setDate(new Date().getDate() - 2))
-let day4 = new Date(new Date().setDate(new Date().getDate() - 3))
-let day3 = new Date(new Date().setDate(new Date().getDate() - 4))
-let day2 = new Date(new Date().setDate(new Date().getDate() - 5))
-let day1 = new Date(new Date().setDate(new Date().getDate() - 6))
+let day7 = new Date(environment.today);
+let day6 = new Date(environment.today);
+let day5 = new Date(environment.today);
+let day4 = new Date(environment.today);
+let day3 = new Date(environment.today);
+let day2 = new Date(environment.today);
+
+let days: Date[] = [
+    day7, day6, day5, day4, day3
+];
+let isWeekend = false;
+for(let i = 1; i < days.length; i++){
+days[i].setDate(days[i].getDate() - i);
+
+if(isWeekend){
+    days[i].setDate(days[i].getDate() - 2);
+}
+while(days[i].getDay() === 6 ||days[i].getDay() === 0){
+    days[i].setDate(days[i].getDate() - 1);
+isWeekend = true;
+}
+}
+
 export const MockData: Day[] = [
     {
-        date: day7,
-        firstTaskStart:day7,
-        lastTaskEnd:day7,
-        taskCount: 1,
+        date: days[0],
+        firstTaskStart: days[0],
+        lastTaskEnd: days[0],
+        taskCount: 3,
         tasks: [
             {
                 isApproved: true, isRejected: false, events: [
@@ -20,87 +37,122 @@ export const MockData: Day[] = [
                     { quantity: 1, price: 2, eventTypeName: "Overtid 50%", isExpenseType: false, isHoursEventType: true, isAdditionalHoursEventType: false, isWorkHour: true },
 
                 ]
-            }
-        ]
-    }];/*
-        hoursEvents: [
-            { date: day7, quantity: 7.5, price: 2, eventTypeName: "Arbeidstid", isExpenseType: false, isHoursEventType: true, isAdditionalHoursEventType: false, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day7, lastTaskEnd: day7 },
-            { date: day7, quantity: 1, price: 2, eventTypeName: "Overtid 50%", isExpenseType: false, isHoursEventType: true, isAdditionalHoursEventType: false, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day7, lastTaskEnd: day7 },
-        ],
-        expensesEvents: [
-            { date: day7, quantity: 2, price: 2, eventTypeName: "Liftleie dagpris", isExpenseType: true, isHoursEventType: false, isAdditionalHoursEventType: false, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day7, lastTaskEnd: day7 },
-        ],
-        additionalHoursEvents: [
-            { date: day7, quantity: 2.5, price: 2, eventTypeName: "Liftleie timepris", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day7, lastTaskEnd: day7 },
-            { date: day7, quantity: 6, price: 2, eventTypeName: "Løfte", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day7, lastTaskEnd: day7 },
+            },
+            {
+                isApproved: true, isRejected: false, events: [
+                    { quantity: 2, price: 2, eventTypeName: "Liftleie dagpris", isExpenseType: true, isHoursEventType: false, isAdditionalHoursEventType: false, isWorkHour: false },
+                ]
+            },
+            {
+                isApproved: true, isRejected: false, events: [
+                    { quantity: 2.5, price: 2, eventTypeName: "Liftleie timepris", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: false },
+                    { quantity: 6, price: 2, eventTypeName: "Løfte", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: false },
+                ]
+            },
         ]
     },
-
-
     {
-        date: day6,
-        hoursEvents: [
-            { date: day6, quantity: 7.5, price: 2, eventTypeName: "Arbeidstid", isExpenseType: false, isHoursEventType: true, isAdditionalHoursEventType: false, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day6, lastTaskEnd: day6 },
-            { date: day6, quantity: 1, price: 2, eventTypeName: "Overtid 50%", isExpenseType: false, isHoursEventType: true, isAdditionalHoursEventType: false, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day6, lastTaskEnd: day6 },
-        ],
-        expensesEvents: [
-            { date: day6, quantity: 2, price: 2, eventTypeName: "Liftleie dagpris", isExpenseType: true, isHoursEventType: false, isAdditionalHoursEventType: false, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day6, lastTaskEnd: day6 },
-        ],
-        additionalHoursEvents: [
-            { date: day6, quantity: 2.5, price: 2, eventTypeName: "Liftleie timepris", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day6, lastTaskEnd: day6 },
-            { date: day6, quantity: 6, price: 2, eventTypeName: "Løfte", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day6, lastTaskEnd: day6 },
+        date: days[1],
+        firstTaskStart: days[1],
+        lastTaskEnd: days[1],
+        taskCount: 3,
+        tasks: [
+            {
+                isApproved: true, isRejected: false, events: [
+                    { quantity: 7.5, price: 2, eventTypeName: "Arbeidstid", isExpenseType: false, isHoursEventType: true, isAdditionalHoursEventType: false, isWorkHour: true },
+                    { quantity: 1, price: 2, eventTypeName: "Overtid 50%", isExpenseType: false, isHoursEventType: true, isAdditionalHoursEventType: false, isWorkHour: true },
+
+                ]
+            },
+            {
+                isApproved: true, isRejected: false, events: [
+                    { quantity: 2, price: 2, eventTypeName: "Liftleie dagpris", isExpenseType: true, isHoursEventType: false, isAdditionalHoursEventType: false, isWorkHour: false },
+                ]
+            },
+            {
+                isApproved: true, isRejected: false, events: [
+                    { quantity: 2.5, price: 2, eventTypeName: "Liftleie timepris", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: false },
+                    { quantity: 6, price: 2, eventTypeName: "Løfte", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: false },
+                ]
+            },
         ]
     },
-
-
     {
-        date: day5,
-        hoursEvents: [
-            { date: day5, quantity: 7.5, price: 2, eventTypeName: "Arbeidstid", isExpenseType: false, isHoursEventType: true, isAdditionalHoursEventType: false, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day5, lastTaskEnd: day5 },
-            { date: day5, quantity: 1, price: 2, eventTypeName: "Overtid 50%", isExpenseType: false, isHoursEventType: true, isAdditionalHoursEventType: false, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day5, lastTaskEnd: day5 },
-        ],
-        expensesEvents: [
-            { date: day5, quantity: 2, price: 2, eventTypeName: "Liftleie dagpris", isExpenseType: true, isHoursEventType: false, isAdditionalHoursEventType: false, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day5, lastTaskEnd: day5 },
-        ],
-        additionalHoursEvents: [
-            { date: day5, quantity: 2.5, price: 2, eventTypeName: "Liftleie timepris", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day5, lastTaskEnd: day5 },
-            { date: day5, quantity: 6, price: 2, eventTypeName: "Løfte", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day5, lastTaskEnd: day5 },
+        date: days[2],
+        firstTaskStart: days[2],
+        lastTaskEnd: days[2],
+        taskCount: 3,
+        tasks: [
+            {
+                isApproved: true, isRejected: false, events: [
+                    { quantity: 7.5, price: 2, eventTypeName: "Arbeidstid", isExpenseType: false, isHoursEventType: true, isAdditionalHoursEventType: false, isWorkHour: true },
+                    { quantity: 1, price: 2, eventTypeName: "Overtid 50%", isExpenseType: false, isHoursEventType: true, isAdditionalHoursEventType: false, isWorkHour: true },
+
+                ]
+            },
+            {
+                isApproved: true, isRejected: false, events: [
+                    { quantity: 2, price: 2, eventTypeName: "Liftleie dagpris", isExpenseType: true, isHoursEventType: false, isAdditionalHoursEventType: false, isWorkHour: false },
+                ]
+            },
+            {
+                isApproved: true, isRejected: false, events: [
+                    { quantity: 2.5, price: 2, eventTypeName: "Liftleie timepris", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: false },
+                    { quantity: 6, price: 2, eventTypeName: "Løfte", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: false },
+                ]
+            },
         ]
     },
-
-
-
     {
-        date: day4,
-        hoursEvents: [
-            { date: day4, quantity: 7.5, price: 2, eventTypeName: "Arbeidstid", isExpenseType: false, isHoursEventType: true, isAdditionalHoursEventType: false, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day4, lastTaskEnd: day4 },
-            { date: day4, quantity: 1, price: 2, eventTypeName: "Overtid 50%", isExpenseType: false, isHoursEventType: true, isAdditionalHoursEventType: false, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day4, lastTaskEnd: day4 },
-        ],
-        expensesEvents: [
-            { date: day4, quantity: 2, price: 2, eventTypeName: "Liftleie dagpris", isExpenseType: true, isHoursEventType: false, isAdditionalHoursEventType: false, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day4, lastTaskEnd: day4 },
-        ],
-        additionalHoursEvents: [
-            { date: day4, quantity: 2.5, price: 2, eventTypeName: "Liftleie timepris", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day4, lastTaskEnd: day4 },
-            { date: day4, quantity: 6, price: 2, eventTypeName: "Løfte", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day4, lastTaskEnd: day4 },
+        date: days[3],
+        firstTaskStart: days[3],
+        lastTaskEnd: days[3],
+        taskCount: 3,
+        tasks: [
+            {
+                isApproved: true, isRejected: false, events: [
+                    { quantity: 7.5, price: 2, eventTypeName: "Arbeidstid", isExpenseType: false, isHoursEventType: true, isAdditionalHoursEventType: false, isWorkHour: true },
+                    { quantity: 1, price: 2, eventTypeName: "Overtid 50%", isExpenseType: false, isHoursEventType: true, isAdditionalHoursEventType: false, isWorkHour: true },
+
+                ]
+            },
+            {
+                isApproved: true, isRejected: false, events: [
+                    { quantity: 2, price: 2, eventTypeName: "Liftleie dagpris", isExpenseType: true, isHoursEventType: false, isAdditionalHoursEventType: false, isWorkHour: false },
+                ]
+            },
+            {
+                isApproved: true, isRejected: false, events: [
+                    { quantity: 2.5, price: 2, eventTypeName: "Liftleie timepris", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: false },
+                    { quantity: 6, price: 2, eventTypeName: "Løfte", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: false },
+                ]
+            },
         ]
     },
-
-
-
     {
-        date: day3,
-        hoursEvents: [
-            { date: day3, quantity: 7.5, price: 2, eventTypeName: "Arbeidstid", isExpenseType: false, isHoursEventType: true, isAdditionalHoursEventType: false, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day3, lastTaskEnd: day3 },
-            { date: day3, quantity: 1, price: 2, eventTypeName: "Overtid 50%", isExpenseType: false, isHoursEventType: true, isAdditionalHoursEventType: false, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day3, lastTaskEnd: day3 },
-        ],
-        expensesEvents: [
-            { date: day3, quantity: 2, price: 2, eventTypeName: "Liftleie dagpris", isExpenseType: true, isHoursEventType: false, isAdditionalHoursEventType: false, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day3, lastTaskEnd: day3 },
-        ],
-        additionalHoursEvents: [
-            { date: day3, quantity: 2.5, price: 2, eventTypeName: "Liftleie timepris", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day3, lastTaskEnd: day3 },
-            { date: day3, quantity: 6, price: 2, eventTypeName: "Løfte", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: true, isApproved: true, isRejected: false, tasksCount: 3, firstTaskStart: day3, lastTaskEnd: day3 },
-        ]
-    },
+        date: days[4],
+        firstTaskStart: days[4],
+        lastTaskEnd: days[4],
+        taskCount: 3,
+        tasks: [
+            {
+                isApproved: true, isRejected: false, events: [
+                    { quantity: 7.5, price: 2, eventTypeName: "Arbeidstid", isExpenseType: false, isHoursEventType: true, isAdditionalHoursEventType: false, isWorkHour: true },
+                    { quantity: 1, price: 2, eventTypeName: "Overtid 50%", isExpenseType: false, isHoursEventType: true, isAdditionalHoursEventType: false, isWorkHour: true },
 
-]; 
-*/
+                ]
+            },
+            {
+                isApproved: true, isRejected: false, events: [
+                    { quantity: 2, price: 2, eventTypeName: "Liftleie dagpris", isExpenseType: true, isHoursEventType: false, isAdditionalHoursEventType: false, isWorkHour: false },
+                ]
+            },
+            {
+                isApproved: true, isRejected: false, events: [
+                    { quantity: 2.5, price: 2, eventTypeName: "Liftleie timepris", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: false },
+                    { quantity: 6, price: 2, eventTypeName: "Løfte", isExpenseType: false, isHoursEventType: false, isAdditionalHoursEventType: true, isWorkHour: false },
+                ]
+            },
+        ]
+    }
+];
