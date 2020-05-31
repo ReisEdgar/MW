13cafc0ea8ef9b86edbcab3e1be5e9774956352e
ReisEdgar <edgar.reis447@gmail.com>
2020-05-31 11:52:13 +0300

    add base funcionality to calendar component

--- src/app/Components/calendar/calendar.component.ts
+++ src/app/Components/calendar/calendar.component.ts
@@ -10,11 +10,20 @@ import { DaySlot } from './models/day-slot';
 export class CalendarComponent implements OnInit {
   
   @Input() days: DaySlot[];
-
-  constructor() {    
+  selectedDate:Date;
+  constructor() {   
+    this.days = this.sortByDate(this.days);
+    let today = this.days.find(x => x.isToday);
+    this.selectedDate = new Date(today.date);
    }
 
   ngOnInit(): void {
   }
 
+  private sortByDate(days: DaySlot[]): DaySlot[] {
+    days.sort((a: DaySlot, b: DaySlot) => {
+      return a.date.getTime() - b.date.getTime();
+    });
+    return days;
+  }
 }
